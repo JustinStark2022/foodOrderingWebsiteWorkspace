8989940edcc37742c0fb9b7592c8f8bfc667c099
ekimt27 <ekimt27@gmail.com>
2023-04-27 22:23:20 -0600

    Logout button now redirects to index.

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def admin():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('login'))
+    return redirect(url_for('index'))
     
 @app.route('/image/<filename>')
 def image(filename):
